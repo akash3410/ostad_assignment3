9dd2d4399db344acb2be341ad344b0e9ce9792e7
Arifuzzaman Akash <mdarifuzzaman982k@gmail.com>
2024-09-17 15:33:41 +0600

    add

--- myapp/forms.py
+++ myapp/forms.py
@@ -5,3 +5,12 @@ class UpdateForm(forms.ModelForm):
     class Meta:
         model = Employee
         fields = ['name', 'address', 'phone', 'short_description']
+
+class AddEmployee(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['name', 'address', 'phone', 'salary', 'designation', 'short_description']
+        widgets = {
+            'address': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+            'short_description': forms.Textarea(attrs={'rows': 3, 'cols': 40}),
+        }
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Employee
-from .forms import UpdateForm
+from .forms import UpdateForm, AddEmployee
 
 # Create your views here.
 def home_page(request):
@@ -24,3 +24,17 @@ def update_emp(request, pk):
         form = UpdateForm(instance=employee)
 
     return render(request, 'update_emp.html', {'form': form})
+
+def add_emp(request):
+    if request.method == 'POST':
+        form = AddEmployee(request.POST)
+        if form.is_valid():
+            employee = form.save(commit=False)
+            employee.salary = form.cleaned_data['salary']
+            employee.designation = form.cleaned_data['designation']
+            employee.save()
+            return redirect('home_page')
+    else:
+        form = AddEmployee()
+
+    return render(request, 'add_emp.html', {'form': form})
